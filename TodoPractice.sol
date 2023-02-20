0a4640a95ecacb4bc1faeb763fcd069fb66cf539
stevecyj <ycsteven.tsao@gmail.com>
2023-02-21 07:55:13 +0800

    WIX:declare function name

--- TodoPractice.sol
+++ TodoPractice.sol
@@ -2,6 +2,46 @@
 
 pragma solidity >=0.8.4;
 
-contract TodoList{
+contract TodoList {
+    //    增加代辦事項，array
+    string[] public todos;
+    //    增加已完成事項
+    string[] public todoCompleted;
 
+    /**
+     * 新增 todo
+     */
+    function addTodo() public {}
+
+    /**
+     * 刪除 todo
+     */
+    function delTodo() public {
+
+    }
+    /**
+     * 查看指定 todo
+     */
+    function getTodo() public {
+
+    }
+
+    /**
+     * 查看所有 todo
+     */
+    function getAllTodo() public {
+
+    }
+    /**
+     * todo 改為 completed
+     */
+    function setCompleted() public {
+
+    }
+
+    /**
+     * 查看所有 completed
+     */
+    function getAllTodoComplete() public {
+    }
 }
