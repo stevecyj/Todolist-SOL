7e8494432ab6d9aee1aba946e595c5929f278205
Steve <ycsteven.tsao@gmail.com>
2023-02-21 23:17:45 +0800

    fix: addTodo

--- TodoPractice.sol
+++ TodoPractice.sol
@@ -4,14 +4,16 @@ pragma solidity >=0.8.4;
 
 contract TodoList {
     //    增加代辦事項，array
-    string[] public todos;
+    string[] public todos; // storage
     //    增加已完成事項
     string[] public todoCompleted;
 
     /**
      * 新增 todo
      */
-    function addTodo(string todo) public {}
+    function addTodo(string memory todo) public {
+        todos.push(todo);
+    }
 
     /**
      * 刪除 todo
