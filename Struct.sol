b7666fc24ee475d61f7d8dc2a1214803686b13ed
Steve <ycsteven.tsao@gmail.com>
2023-02-22 17:25:23 +0800

    struct,不用照順序

--- Struct.sol
+++ Struct.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >= 0.8.4;
+pragma solidity >=0.8.4;
 
 contract Struct {
     struct User {
@@ -11,7 +11,10 @@ contract Struct {
     User owner;
 
     constructor() {
-        owner = User(msg.sender, "JOJOworld");
+        owner = User({
+        name : "JOJOworld",
+        id : msg.sender
+        });
     }
 
     function getOwnerAddress() public view returns (address) {
