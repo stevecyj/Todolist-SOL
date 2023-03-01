726fdcaae38b675e16b4f41b5d8f3eb539695e87
Steve <ycsteven.tsao@gmail.com>
2023-03-01 16:14:33 +0800

    public and private

--- Visibility.sol
+++ Visibility.sol
@@ -2,7 +2,17 @@
 
 pragma solidity >= 0.8.4;
 
-contract Visibility {
+contract Math {
+//    function addOne(uint256 x) public pure returns (uint256){
+//        return x + 1;
+//    }
+
+    function addOne(uint256 x) private pure returns (uint256){
+        return x + 1;
+    }
+}
+
+contract Visibility is Math {
     function coreFunction() public pure returns (string memory){
         return 'Hello World!';
     }
